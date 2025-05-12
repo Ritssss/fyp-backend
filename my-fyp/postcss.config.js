c422d07fcad060fee328914cee14826c445d187b
dolkar33 <dolkarghale444@gmail.com>
2025-05-12 20:55:43 +0545

    Initial commit with tailwind

---
+++ my-fyp/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: [
+    require('tailwindcss'),       // Direct Tailwind CSS plugin
+    require('autoprefixer'),      // Autoprefixer to add vendor prefixes
+  ],
+}
---
+++ my-fyp/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
