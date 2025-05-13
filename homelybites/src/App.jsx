2e2508c6487290121919c382faad5e3174df2a6a
dolkar33 <dolkarghale444@gmail.com>
2025-05-13 21:00:20 +0545

    Vite frontend and tailwind

---
+++ homelybites/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-pink-300">Homely Bites</h1>
+    </>
+  )
+}
+
+export default App
--- my-fyp/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: [
-    require('tailwindcss'),       // Direct Tailwind CSS plugin
-    require('autoprefixer'),      // Autoprefixer to add vendor prefixes
-  ],
-}
