6e25cb508ef39514c8b083dea3d33867b3e2de0e
Bishesh <bisheshshakya123@gmail.com>
2025-05-25 14:34:59 +0545

    Add ContactUs Page

--- homelybites/src/App.jsx
+++ homelybites/src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/LandingPage";
 import UserQuestion from "./pages/UserQuestion";
 import Login from "./pages/login";
 import MainPage from "./pages/MainPage";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
       <Route path="/signup" element={<SignUp />} />
       <Route path="/userquestion" element={<UserQuestion />} />
       <Route path="/LandingPage" element={<LandingPage />} />
+      <Route path="/contact" element={<ContactUs />} />
       <Route path="/" element={<MainPage />} />
+      {/*This one should be at the end always */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
--- homelybites/tailwind.config.js
+++ homelybites/tailwind.config.js
@@ -2,12 +2,14 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
   ],
   theme: {
     extend: {
       colors: {
         accent: "#FC7D7D",
+        vrless: "#FFE7E7",
+        greyy:"#857F7F",
       },
       fontFamily: {
         amaranth: ['Amaranth', 'sans-serif'],
